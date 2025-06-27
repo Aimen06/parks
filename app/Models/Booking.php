99633a88a07f44b2ce0ebc402436a8fb23de5cb6
Aïmen <aimen2nice@gmail.com>
2025-06-27 10:50:22 +0200

    model ok

---
+++ app/Models/Booking.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Booking extends Model
+{
+    /** @use HasFactory<\Database\Factories\BookingFactory> */
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'user_id',
+        'parking_id',
+        'entry_time',
+        'exit_time',
+        'duration',
+        'price',
+    ];
+
+    protected $casts = [
+        'entry_time' => 'datetime',
+        'exit_time' => 'datetime',
+        'duration' => 'integer'
+    ];
+
+    // Une réservation concerne un parking
+    public function parking()
+    {
+        return $this->belongsTo(Parking::class);
+    }
+
+    // Une réservation peut avoir une review
+    public function review()
+    {
+        return $this->hasOne(Review::class);
+    }
+
+    // Une réservation peut être associée à un paiement
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+}
