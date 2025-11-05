3a3b1cd014392a7adc4fba84be680f5489973798
Aïmen <aimen2nice@gmail.com>
2025-11-05 01:07:55 +0100

    wip register

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -10,18 +10,28 @@ import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
 
 type RegisterForm = {
-    name: string;
+    firstname: string;
+    lastname: string;
     email: string;
     password: string;
     password_confirmation: string;
+    address: string;
+    zipcode: string;
+    city: string;
+    rgpd: boolean;
 };
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm<Required<RegisterForm>>({
-        name: '',
+        firstname: '',
+        lastname: '',
         email: '',
         password: '',
         password_confirmation: '',
+        address: '',
+        zipcode: '',
+        city: '',
+        rgpd: false,
     });
 
     const submit: FormEventHandler = (e) => {
@@ -32,29 +42,45 @@ export default function Register() {
     };
 
     return (
-        <AuthLayout title="Create an account" description="Enter your details below to create your account">
+        <AuthLayout title="Créer un compte" description="Saisir vos informations pour créer un compte.">
             <Head title="Register" />
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
-                        <Label htmlFor="name">Name</Label>
+                        <Label htmlFor="firstname">Nom</Label>
                         <Input
-                            id="name"
+                            id="firstname"
                             type="text"
                             required
                             autoFocus
                             tabIndex={1}
-                            autoComplete="name"
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
+                            autoComplete="firstname"
+                            value={data.firstname}
+                            onChange={(e) => setData('firstname', e.target.value)}
                             disabled={processing}
-                            placeholder="Full name"
+                            placeholder="Dupont"
                         />
-                        <InputError message={errors.name} className="mt-2" />
+                        <InputError message={errors.firstname} className="mt-2" />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="firstname">Prénom</Label>
+                        <Input
+                            id="lastname"
+                            type="text"
+                            required
+                            autoFocus
+                            tabIndex={1}
+                            autoComplete="lastname"
+                            value={data.lastname}
+                            onChange={(e) => setData('lastname', e.target.value)}
+                            disabled={processing}
+                            placeholder="Tom"
+                        />
+                        <InputError message={errors.lastname} className="mt-2" />
                     </div>
 
                     <div className="grid gap-2">
-                        <Label htmlFor="email">Email address</Label>
+                        <Label htmlFor="email">Email</Label>
                         <Input
                             id="email"
                             type="email"
@@ -64,13 +90,13 @@ export default function Register() {
                             value={data.email}
                             onChange={(e) => setData('email', e.target.value)}
                             disabled={processing}
-                            placeholder="email@example.com"
+                            placeholder="dupont.tom@gmail.com"
                         />
                         <InputError message={errors.email} />
                     </div>
 
                     <div className="grid gap-2">
-                        <Label htmlFor="password">Password</Label>
+                        <Label htmlFor="password">Mot de passe</Label>
                         <Input
                             id="password"
                             type="password"
@@ -80,13 +106,13 @@ export default function Register() {
                             value={data.password}
                             onChange={(e) => setData('password', e.target.value)}
                             disabled={processing}
-                            placeholder="Password"
+                            placeholder="******"
                         />
                         <InputError message={errors.password} />
                     </div>
 
                     <div className="grid gap-2">
-                        <Label htmlFor="password_confirmation">Confirm password</Label>
+                        <Label htmlFor="password_confirmation">Confirmation du passe</Label>
                         <Input
                             id="password_confirmation"
                             type="password"
@@ -96,21 +122,70 @@ export default function Register() {
                             value={data.password_confirmation}
                             onChange={(e) => setData('password_confirmation', e.target.value)}
                             disabled={processing}
-                            placeholder="Confirm password"
+                            placeholder="******"
                         />
                         <InputError message={errors.password_confirmation} />
                     </div>
 
+                    <div className="grid gap-2">
+                        <Label htmlFor="address">Adresse</Label>
+                        <Input
+                            id="address"
+                            type="text"
+                            required
+                            tabIndex={4}
+                            autoComplete="address"
+                            value={data.address}
+                            onChange={(e) => setData('address', e.target.value)}
+                            disabled={processing}
+                            placeholder="2 rue de la paix"
+                        />
+                        <InputError message={errors.address} />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="zipcode">Code postal</Label>
+                        <Input
+                            id="zipcode"
+                            type="text"
+                            required
+                            tabIndex={4}
+                            inputMode="numeric"
+                            autoComplete="postal-code"
+                            pattern="\d{5}"
+                            maxLength={5}
+                            value={data.zipcode}
+                            onChange={(e) => setData('zipcode', e.target.value)}
+                            disabled={processing}
+                            placeholder="06000"
+                        />
+                        <InputError message={errors.zipcode} />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="address">Ville</Label>
+                        <Input
+                            id="city"
+                            type="text"
+                            required
+                            tabIndex={4}
+                            autoComplete="city"
+                            value={data.city}
+                            onChange={(e) => setData('city', e.target.value)}
+                            disabled={processing}
+                            placeholder="Nice"
+                        />
+                        <InputError message={errors.city} />
+                    </div>
+
                     <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Create account
+                        Créer mon compte
                     </Button>
                 </div>
 
                 <div className="text-muted-foreground text-center text-sm">
-                    Already have an account?{' '}
+                    Vous avez déjà un compte?{' '}
                     <TextLink href={route('login')} tabIndex={6}>
-                        Log in
+                        Connexion
                     </TextLink>
                 </div>
             </form>
